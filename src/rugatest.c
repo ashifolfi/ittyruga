1e8f44141fdd0b1456dd9741bae9611a002e9045
ash <anticream39@yahoo.com>
2022-10-18 21:41:30 -0400

    remove placeholder example files

--- src/rugatest.c
+++ src/rugatest.c
@@ -3,14 +3,12 @@
 #include "contrib/gbamap.h"
 
 Sprite* ship;
-//const int WALK_SPEED = 1;
+const int WALK_SPEED = 1;
 
 /** virtual position */
 typedef struct VPos {
     short x, y;
 } VPos;
-
-VPos ship_vpos;
 // background position in relation to that of the rest of the game
 VPos bg_vpos;
 
@@ -37,11 +35,10 @@ void rugaroom_update() {
     // input
     int y_move = key_tri_vert();
     int x_move = key_tri_horz();
-    //bool moving = (y_move != 0 || x_move != 0);
 
     // update pos
-    ship->x += x_move * 1;
-    ship->y += y_move * 1;
+    ship->x += x_move * WALK_SPEED;
+    ship->y += y_move * WALK_SPEED;
 
     // update sprites
     dusk_sprites_update();
--- src/scenes.h
+++ src/scenes.h
@@ -1,5 +1,4 @@
 #pragma once
 
 extern Scene logo_scene;
-extern Scene fountain_scene;
 extern Scene rugatest_scene;
